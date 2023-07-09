979835e8e197fa9c70e2beae5f7ec78d4b06c90c
Amit Halevi <77746878+HaleviAmit@users.noreply.github.com>
2023-07-09 18:28:41 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ const connect = async () => {
 
 connect();
 app.use(cors({
-  origin: ["project-business-cards-ela5.vercel.app"],
+  origin: ["project-business-cards.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true
 }));
